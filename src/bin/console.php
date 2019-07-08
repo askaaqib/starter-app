f1f5074d31e2682993b48257e0dfc8b8ee2c3250
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-07-08 22:29:03 +0500

    default bin script

---
+++ src/bin/console.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace bin;
+
+use Comely\App\CLI\AbstractAppScript;
+
+/**
+ * Class console
+ * @package bin
+ */
+class console extends AbstractAppScript
+{
+    public function exec(): void
+    {
+
+    }
+}
